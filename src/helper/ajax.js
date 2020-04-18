a11d2898f820a1491efc08547172ff7d82c01b73
encodedBicoding <dominicisioma000@gmail.com>
2020-04-18 01:27:43 +0100

    update meta tags

--- src/helper/ajax.js
+++ src/helper/ajax.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://api-hwdykm.herokuapp.com/api/v1';
+const BASE_URL = 'http://api-hwdykm.herokuapp.com/api/v1';
 export default BASE_URL;
 // 'https://api-hwdykm.herokuapp.com/api/v1'
